9b3d4c42fa2298062227539ba47dd8e67198c444
Gurgen <1gurgenmuradyan@gmail.com>
2022-05-24 01:34:50 +0300

    update apple file

--- enums/appleSite.js
+++ enums/appleSite.js
@@ -1,13 +1,9 @@
 module.exports = {
-    'apple_app_site_association': {
-        'applinks': {
-            'apps': [],
-            'details': [
-                {
-                    'appID': '4BGD9CUNK.page.crypto.crypto.page',
-                    'paths': ['https://crypto-page-app.herokuapp.com/login/*']
-                }
-            ]
-        }
+    'applinks': {
+        'apps': [],
+        'details': [{
+            'appID': '4BGD9CUNK.page.crypto.crypto.page',
+            'paths': ['https://crypto-page-app.herokuapp.com/login/*']
+        }]
     }
 }
