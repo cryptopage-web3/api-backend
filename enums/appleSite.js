efc8e0010a35dc04238a4d64d8c5e3e704d1ad41
Gurgen <1gurgenmuradyan@gmail.com>
2022-05-24 01:00:31 +0300

    [FEATURE] Add applesite json

---
+++ enums/appleSite.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'apple-app-site-association': {
+        'applinks': {
+            'apps': [],
+            'details': [
+                {
+                    'appID': '4BGD9CUNK.page.crypto.crypto.page',
+                    'paths': ['https://crypto-page-app.herokuapp.com/login/*']
+                }
+            ]
+        }
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,25 @@
 const express = require('express');
 const router = express.Router();
 
+const appleSiteJson = require('../enums/appleSite');
+
 const BAD_REQUEST = 400;
 
 router.get('/health-check', async (_, res) => {
 
     try {
         res.json({ success: true });
+    } catch (err) {
+        res.status(BAD_REQUEST).json({
+            message: err.message
+        });
+    }
+});
+
+router.get('/login*', async (_, res) => {
+
+    try {
+        res.json(appleSiteJson);
     } catch (err) {
         res.status(BAD_REQUEST).json({
             message: err.message
