ed9297a20179fbc5048fdf6d7e88dd6b39270081
evhen <x500dll@gmai.com>
2022-06-10 13:27:24 +0300

    collections router moved to routes root dir

--- src/routes/collections.ts
+++ src/routes/collections.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { FindOptions, InferAttributes } from "sequelize/types";
-import { NftCollection } from "../../../orm/model/nftcollection";
+import { NftCollection } from "../orm/model/nftcollection";
 
 export const collectionsRouter = Router();
 
