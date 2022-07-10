661287f1fbd46111ef6db08e36ac48692fec2bee
Gurgen <1gurgenmuradyan@gmail.com>
2022-07-10 21:56:14 +0300

    [FIX] Fix nft assets skip limit

--- src/routes/nfts.js
+++ src/routes/nfts.js
@@ -50,7 +50,7 @@ router.get('/:chain/:address', async (req, res) => {
     const { skip = 0, limit = 20 } = req.query;
 
     try {
-        const { list, count } = await nftsModule.getWalletAllNFTs(address, chain, Number(skip)++, Number(limit));
+        const { list, count } = await nftsModule.getWalletAllNFTs(address, chain, Number(skip)+1, Number(limit));
         res.json({ list, count });
     } catch (err) {
         res.status(BAD_REQUEST).json({
