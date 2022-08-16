09da469ef4595053cd072247f2e15fe7d2614888
di-djon <x500dll@gmail.com>
2022-08-16 15:18:45 +0300

    tests moved to mocha + sinon

--- test/integration/api/nfts.test.ts
+++ test/integration/api/nfts.test.ts
@@ -1,166 +1,158 @@
-require('dotenv').config()
-
-import "reflect-metadata"
-
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { agent } from "supertest";
 import { container } from '../../../src/ioc';
 import { Axios } from 'axios';
 import { IDS } from '../../../src/types/index';
 import { unmarshalEthNftsResponse, unmarshalNftTransactionsEmptyResponse, unmarshalEthNftTransactionsResponse, unmarshalMaticNftsResponse, unmarshalBscNtsResponse, unmarshalNtsEmptyResponse, unmarshalMaticNftTransactionsResponse, unmarshalBscNfttransactionsResponse } from './nfts-response';
+import Sinon from 'sinon';
+import { expect } from 'chai';
 
 const app = new InversifyExpressServer(container).build()
 const testAgent = agent(app)
 
-jest.mock('axios')
-
 const axios:Axios = container.get(IDS.NODE_MODULES.axios)
-const axiosGetMock = axios.get as jest.Mock
+const axiosGetStub = Sinon.stub(axios, 'get')
 
 describe('test nfts api endpoints', ()=>{
-    afterEach(()=>{
-        jest.resetAllMocks()
-    })
-
     it('should return eth nfts', async ()=>{
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalEthNftsResponse})
+        axiosGetStub
+            .resolves({data: unmarshalEthNftsResponse})
 
         const response = await testAgent
             .get('/nfts/eth/0xb29c388e3fd63e1050ac5e4ca1d046dca36f004c')
             .expect('Content-Type',/json/)
 
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.count).toBe(13)
-            expect(response.body.list.length).toBe(10)
-            expect(response.body.list[0]).toEqual(expect.objectContaining({
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.count).to.eq(13)
+            expect(response.body.list.length).to.eq(10)
+            expect(response.body.list[0]).to.contain(({
                 from: '0x8b27de7f6a7542ee70e2420e1bc67fc479d01984',
                 to: '0xb29c388e3fd63e1050ac5e4ca1d046dca36f004c',
                 //name: 'v1 punk #8561',
                 tokenId: '8561'
             }))
 
-            expect((axios.get as jest.Mock).mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return polygon nfts', async () => {
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalMaticNftsResponse})
+        axiosGetStub
+            .resolves({data: unmarshalMaticNftsResponse})
 
         const response = await testAgent
             .get('/nfts/matic/0xBA7089b207205c1B2282A18c1C80E856Fd424de0')
             .expect('Content-Type',/json/)
 
-            expect(response.body.count).toBe(102)
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.list.length).toBe(4)
-            expect(response.body.list[0]).toEqual(expect.objectContaining({
+            expect(response.body.count).to.eq(102)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.list.length).to.eq(4)
+            expect(response.body.list[0]).to.contain(({
                 contract_address: '0x2953399124f0cbb46d2cbacd8a89cf0599974963',
                 from: '0xb7f8d0b571f9fc79e896c778ccdff2f92279ac21',
                 to: '0xBA7089b207205c1B2282A18c1C80E856Fd424de0',
                 tokenId: '1267276096787188443380565843485598748588859320258549594955629332512307718469'
             }))
 
-            expect(axiosGetMock.mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return bsc nfts', async () => {
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalBscNtsResponse})
+        axiosGetStub
+            .resolves({data: unmarshalBscNtsResponse})
 
         const response = await testAgent
             .get('/nfts/bsc/0x2465176C461AfB316ebc773C61fAEe85A6515DAA')
             .expect('Content-Type',/json/)
 
-            expect(response.body.count).toBe(2)
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.list.length).toBe(2)
-            expect(response.body.list[0]).toEqual(expect.objectContaining({
+            expect(response.body.count).to.eq(2)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.list.length).to.eq(2)
+            expect(response.body.list[0]).to.contain(({
                 contract_address: '0x3d7b0001e03096d3795fd5d984ad679467546d73',
                 from: '0x3c284a074afe106adbc1b6eeea1c15983eafdc47',
                 to: '0x2465176C461AfB316ebc773C61fAEe85A6515DAA',
                 tokenId: '166401'
             }))
 
-            expect(axiosGetMock.mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should not return error eth nfts', async ()=>{
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalNtsEmptyResponse})
+        axiosGetStub
+            .resolves({data: unmarshalNtsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/eth/0xb29c388e3fd63e1050ac5e4ca1d046dca36f004c')
             .expect('Content-Type',/json/)
 
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.count).toBe(0)
-            expect(response.body.list.length).toBe(0)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.count).to.eq(0)
+            expect(response.body.list.length).to.eq(0)
 
-            expect((axios.get as jest.Mock).mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should not return error polygon nfts', async () => {
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalNtsEmptyResponse})
+        axiosGetStub
+            .resolves({data: unmarshalNtsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/matic/0xBA7089b207205c1B2282A18c1C80E856Fd424de0')
             .expect('Content-Type',/json/)
 
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.count).toBe(0)
-            expect(response.body.list.length).toBe(0)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.count).to.eq(0)
+            expect(response.body.list.length).to.eq(0)
 
-            expect(axiosGetMock.mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should not return error bsc nfts', async () => {
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalNtsEmptyResponse})
+        axiosGetStub
+            .resolves({data: unmarshalNtsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/bsc/0x2465176C461AfB316ebc773C61fAEe85A6515DAA')
             .expect('Content-Type',/json/)
 
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.count).toBe(0)
-            expect(response.body.list.length).toBe(0)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.count).to.eq(0)
+            expect(response.body.list.length).to.eq(0)
 
-            expect(axiosGetMock.mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return eth nft transactions', async ()=>{
-        axiosGetMock.mockResolvedValue({data: unmarshalEthNftTransactionsResponse})
+        axiosGetStub.resolves({data: unmarshalEthNftTransactionsResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/eth/0x2aDe7E7ed11a4E35C2dDCCB6189d4fE710A165f5')
             .expect('Content-Type',/json/)
 
-        expect(Array.isArray(response.body.list)).toBe(true)
-        expect(response.body.count).toBe(24)
-        expect(response.body.list.length).toBe(10)
-        expect(response.body.list[0]).toEqual(expect.objectContaining({
+        expect(Array.isArray(response.body.list)).to.eq(true)
+        expect(response.body.count).to.eq(24)
+        expect(response.body.list.length).to.eq(10)
+        expect(response.body.list[0]).to.contain(({
             from: '0xb000953624c10427ad028510cf3249388ffdf310',
             to: '0xb29c388e3fd63e1050ac5e4ca1d046dca36f004c',
             blockNumber: 15300497,
             tokenId: '86322540947695616051707333350443506684962566151002367173878109827558281315304'
         }))
 
-        expect(axiosGetMock.mock.calls.length).toBe(1)
+        expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return matic nft transactions', async ()=>{
-        axiosGetMock.mockResolvedValue({data: unmarshalMaticNftTransactionsResponse})
+        axiosGetStub.resolves({data: unmarshalMaticNftTransactionsResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/matic/0xBA7089b207205c1B2282A18c1C80E856Fd424de0')
             .expect('Content-Type',/json/)
 
-        expect(Array.isArray(response.body.list)).toBe(true)
-        expect(response.body.count).toBe(116)
-        expect(response.body.list.length).toBe(4)
-        expect(response.body.list[0]).toEqual(expect.objectContaining({
+        expect(Array.isArray(response.body.list)).to.eq(true)
+        expect(response.body.count).to.eq(116)
+        expect(response.body.list.length).to.eq(4)
+        expect(response.body.list[0]).to.contain(({
             type: 'base_info',
             from: '0xae94ccf82b3c8044b31a4fcff2455480763ae226',
             to: '0xba7089b207205c1b2282a18c1c80e856fd424de0',
@@ -168,20 +160,20 @@ describe('test nfts api endpoints', ()=>{
             tokenId: '78965343665950388415519985342127408390054350375949077399659463369044632110752'
         }))
 
-        expect(axiosGetMock.mock.calls.length).toBe(1)
+        expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return bsc nft transactions', async ()=>{
-        axiosGetMock.mockResolvedValue({data: unmarshalBscNfttransactionsResponse})
+        axiosGetStub.resolves({data: unmarshalBscNfttransactionsResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/bsc/0x2465176C461AfB316ebc773C61fAEe85A6515DAA')
             .expect('Content-Type',/json/)
 
-        expect(Array.isArray(response.body.list)).toBe(true)
-        expect(response.body.count).toBe(2)
-        expect(response.body.list.length).toBe(2)
-        expect(response.body.list[0]).toEqual(expect.objectContaining({
+        expect(Array.isArray(response.body.list)).to.eq(true)
+        expect(response.body.count).to.eq(2)
+        expect(response.body.list.length).to.eq(2)
+        expect(response.body.list[0]).to.contain(({
             type: 'base_info',
             from: '0x7bf3fde4fe3d031caf369bfd15855b31b4512a9d',
             to: '0x2465176c461afb316ebc773c61faee85a6515daa',
@@ -189,50 +181,51 @@ describe('test nfts api endpoints', ()=>{
             tokenId: '340282366920938463463374607431768211749'
         }))
 
-        expect(axiosGetMock.mock.calls.length).toBe(1)
+        expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should not return error eth nfts transactions', async ()=>{
-        axiosGetMock
-            .mockResolvedValueOnce({data: unmarshalNftTransactionsEmptyResponse})
+        axiosGetStub
+            .resolves({data: unmarshalNftTransactionsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/eth/0x2aDe7E7ed11a4E35C2dDCCB6189d4fE710A165f5')
             .expect('Content-Type',/json/)
 
-            expect(Array.isArray(response.body.list)).toBe(true)
-            expect(response.body.count).toBe(0)
-            expect(response.body.list.length).toBe(0)
+            expect(Array.isArray(response.body.list)).to.eq(true)
+            expect(response.body.count).to.eq(0)
+            expect(response.body.list.length).to.eq(0)
 
-            expect((axios.get as jest.Mock).mock.calls.length).toBe(1)
+            expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should not return error matic nft transactions', async ()=>{
-        axiosGetMock.mockResolvedValue({data: unmarshalNftTransactionsEmptyResponse})
+        axiosGetStub.resolves({data: unmarshalNftTransactionsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/matic/0xBA7089b207205c1B2282A18c1C80E856Fd424de0')
             .expect('Content-Type',/json/)
 
-        expect(Array.isArray(response.body.list)).toBe(true)
-        expect(response.body.count).toBe(0)
-        expect(response.body.list.length).toBe(0)
+        expect(Array.isArray(response.body.list)).to.eq(true)
+        expect(response.body.count).to.eq(0)
+        expect(response.body.list.length).to.eq(0)
 
-        expect(axiosGetMock.mock.calls.length).toBe(1)
+        expect(axiosGetStub.calledOnce).to.eq(true)
     })
 
     it('should return bsc nft transactions', async ()=>{
-        axiosGetMock.mockResolvedValue({data: unmarshalNftTransactionsEmptyResponse})
+        axiosGetStub.resolves({data: unmarshalNftTransactionsEmptyResponse})
 
         const response = await testAgent
             .get('/nfts/transactions/bsc/0x2465176C461AfB316ebc773C61fAEe85A6515DAA')
             .expect('Content-Type',/json/)
 
-        expect(Array.isArray(response.body.list)).toBe(true)
-        expect(response.body.count).toBe(0)
-        expect(response.body.list.length).toBe(0)
+        expect(Array.isArray(response.body.list)).to.eq(true)
+        expect(response.body.count).to.eq(0)
+        expect(response.body.list.length).to.eq(0)
 
-        expect(axiosGetMock.mock.calls.length).toBe(1)
+        expect(axiosGetStub.calledOnce).to.eq(true)
     })
+
 
 })
--- test/integration/api/tokens.test.ts
+++ test/integration/api/tokens.test.ts
@@ -1,103 +1,96 @@
-require('dotenv').config()
-
-import "reflect-metadata"
-
 import { InversifyExpressServer } from 'inversify-express-utils';
 import { agent } from "supertest";
 import { container } from '../../../src/ioc';
 import { Axios } from 'axios';
 import { IDS } from '../../../src/types/index';
 import { unmarshalEthTokensResponse, unmarshalBscTokensResponse, unmarshalPolygonTokensResponse, covalentSolTokensResponse, tronscanTokensResponse } from './tokens-response';
+import Sinon from 'sinon';
+import { expect } from 'chai';
 
 const app = new InversifyExpressServer(container).build()
 const testAgent = agent(app)
 
-jest.mock('axios')
-
 const axios:Axios = container.get(IDS.NODE_MODULES.axios)
+const axiosGetStub = Sinon.stub(axios,'get')
 
 describe('test tokens api endpoints', ()=>{
-    afterEach(()=>{
-        jest.resetAllMocks()
-    })
-
     it('should return eth tokens', async ()=>{
-        (axios.get as jest.Mock)
-            .mockResolvedValueOnce({data: unmarshalEthTokensResponse})
+        axiosGetStub
+            .resolves({data: unmarshalEthTokensResponse})
 
         const response = await testAgent
             .get('/tokens/eth/0xb29c388e3fd63e1050ac5e4ca1d046dca36f004c')
             .expect('Content-Type',/json/)
         
-            expect(Array.isArray(response.body.tokens)).toBe(true)
-            expect(response.body.count).toBe(4)
-            expect(response.body.tokens.length).toBe(4)
-            expect(response.body.tokens[1].symbol).toBe('USDC')
-            expect(response.body.tokens[1].address).toBe('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
-            expect(response.body.tokens[1].balance).toBe(250870.248787)
+            expect(Array.isArray(response.body.tokens)).to.eq(true)
+            expect(response.body.count).to.eq(4)
+            expect(response.body.tokens.length).to.eq(4)
+            expect(response.body.tokens[1].symbol).to.eq('USDC')
+            expect(response.body.tokens[1].address).to.eq('0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48')
+            expect(response.body.tokens[1].balance).to.eq(250870.248787)
     })
 
     it('should return bsc tokens', async ()=>{
-        (axios.get as jest.Mock)
-            .mockResolvedValueOnce({data: unmarshalBscTokensResponse})
+        axiosGetStub
+            .resolves({data: unmarshalBscTokensResponse})
 
         const response = await testAgent
             .get('/tokens/bsc/0x2465176C461AfB316ebc773C61fAEe85A6515DAA')
             .expect('Content-Type',/json/)
         
-            expect(Array.isArray(response.body.tokens)).toBe(true)
-            expect(response.body.count).toBe(5)
-            expect(response.body.tokens.length).toBe(5)
-            expect(response.body.tokens[1].symbol).toBe('BNB')
-            expect(response.body.tokens[1].address).toBe('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
-            expect(response.body.tokens[1].balance).toBe(2.005001139492612)
+            expect(Array.isArray(response.body.tokens)).to.eq(true)
+            expect(response.body.count).to.eq(5)
+            expect(response.body.tokens.length).to.eq(5)
+            expect(response.body.tokens[1].symbol).to.eq('BNB')
+            expect(response.body.tokens[1].address).to.eq('0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee')
+            expect(response.body.tokens[1].balance).to.eq(2.005001139492612)
     })
 
     it('should return polygon tokens', async ()=>{
-        (axios.get as jest.Mock)
-            .mockResolvedValueOnce({data: unmarshalPolygonTokensResponse})
+        axiosGetStub
+            .resolves({data: unmarshalPolygonTokensResponse})
 
         const response = await testAgent
             .get('/tokens/matic/0xBA7089b207205c1B2282A18c1C80E856Fd424de0')
             .expect('Content-Type',/json/)
         
-            expect(Array.isArray(response.body.tokens)).toBe(true)
-            expect(response.body.count).toBe(5)
-            expect(response.body.tokens.length).toBe(5)
-            expect(response.body.tokens[1].symbol).toBe('MNEP')
-            expect(response.body.tokens[1].address).toBe('0x0b91b07beb67333225a5ba0259d55aee10e3a578')
-            expect(response.body.tokens[1].balance).toBe(300000)
+            expect(Array.isArray(response.body.tokens)).to.eq(true)
+            expect(response.body.count).to.eq(5)
+            expect(response.body.tokens.length).to.eq(5)
+            expect(response.body.tokens[1].symbol).to.eq('MNEP')
+            expect(response.body.tokens[1].address).to.eq('0x0b91b07beb67333225a5ba0259d55aee10e3a578')
+            expect(response.body.tokens[1].balance).to.eq(300000)
     })
 
     it('should return solana tokens', async ()=>{
-        (axios.get as jest.Mock)
-            .mockResolvedValueOnce({data: covalentSolTokensResponse})
+        axiosGetStub
+            .resolves({data: covalentSolTokensResponse})
 
         const response = await testAgent
             .get('/tokens/sol/Bt1rNX3q66Lq5z4Xdzk1nLkLh7wq1yVgTqnk5LHiXvdF')
             .expect('Content-Type',/json/)
         
-            expect(Array.isArray(response.body.tokens)).toBe(true)
-            expect(response.body.count).toBe(2)
-            expect(response.body.tokens.length).toBe(2)
-            expect(response.body.tokens[1].symbol).toBe('SOL')
-            expect(response.body.tokens[1].name).toBe('Solana')
-            expect(response.body.tokens[1].balance).toBe(7.29358033)
+            expect(Array.isArray(response.body.tokens)).to.eq(true)
+            expect(response.body.count).to.eq(2)
+            expect(response.body.tokens.length).to.eq(2)
+            expect(response.body.tokens[1].symbol).to.eq('SOL')
+            expect(response.body.tokens[1].name).to.eq('Solana')
+            expect(response.body.tokens[1].balance).to.eq(7.29358033)
     })
 
     
     it('should return tron tokens', async ()=>{
-        (axios.get as jest.Mock)
-            .mockResolvedValueOnce({data: tronscanTokensResponse})
+        axiosGetStub
+            .resolves({data: tronscanTokensResponse})
 
         const response = await testAgent
             .get('/tokens/tron/TYZ6QbrPaUjv62CoU7za2nVHJ1ZoEJMXah')
             .expect('Content-Type',/json/)
         
-            expect(Array.isArray(response.body.tokens)).toBe(true)
-            expect(response.body.tokens.length).toBe(3)
-            expect(response.body.tokens[0].symbol).toBe('trx')
-            expect(response.body.tokens[0].name).toBe('trx')
-            expect(response.body.tokens[0].balance).toBe(0.000192)
+            expect(Array.isArray(response.body.tokens)).to.eq(true)
+            expect(response.body.tokens.length).to.eq(3)
+            expect(response.body.tokens[0].symbol).to.eq('trx')
+            expect(response.body.tokens[0].name).to.eq('trx')
+            expect(response.body.tokens[0].balance).to.eq(0.000192)
     })
 })
