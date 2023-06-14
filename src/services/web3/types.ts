9983c5d0cdcbbe0cb7429d66598bb30ce031594d
di-djon <x500dll@gmail.com>
2023-06-14 14:45:09 +0200

    fixed build errors

--- src/services/web3/types.ts
+++ src/services/web3/types.ts
@@ -1,8 +1,9 @@
-import { TokenErcCategory, Web3NftTokenData } from '../../modules/nfts/types';
+import { AssetTransfersCategory } from 'alchemy-sdk';
+import { Web3NftTokenData } from '../../modules/nfts/types';
 import { IChainContext } from '../../types/index';
 
 export interface IWeb3Manager extends IChainContext {
     getDateFromBlock(blocknum: number): Promise<Date>
     getFieldFromContract(address: string, key: string)
-    getTokenData(contrctAddress: string, tokenId: string, category?: TokenErcCategory):Promise<Web3NftTokenData>
+    getTokenData(contrctAddress: string, tokenId: string, category?: AssetTransfersCategory):Promise<Web3NftTokenData>
 }
